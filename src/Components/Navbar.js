f14b8c621aa49f280afff72377b9f7919301d708
manojks1999 <9743manoj@gmail.com>
2022-05-28 12:18:01 +0530

    new update

---
+++ src/Components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Select from 'react-select';
+import '../Styles/Navbar.css';
+
+const Navbar = ({userLang, setUserLang, userTheme,
+				setUserTheme}) => {
+	const languages = [
+		{ value: "c", label: "C" },
+		{ value: "cpp", label: "C++" },
+		{ value: "python", label: "Python" },
+		{ value: "java", label: "Java" },
+	];
+	const themes = [
+		{ value: "vs-dark", label: "Dark" },
+		{ value: "light", label: "Light" },
+	]
+	return (
+		<div className="navbar">
+			<h1>BroCode</h1>
+			<Select options={languages} value={userLang}
+					onChange={(e) => setUserLang(e.value)}
+					placeholder={userLang} />
+			<Select options={themes} value={userTheme}
+					onChange={(e) => setUserTheme(e.value)}
+					placeholder={userTheme} />
+		</div>
+	)
+}
+
+export default Navbar
