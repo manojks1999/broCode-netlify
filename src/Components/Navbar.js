99baf7e71b6d6763af9b59f4528eb416e5b470ed
manojks1999 <64463831+manojks1999@users.noreply.github.com>
2022-06-17 23:18:10 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = ({userLang, setUserLang, userTheme,
 	]
 	return (
 		<div className="navbar">
-			<h1>BroCode</h1>
+			<h1>Lets Code</h1>
 			<Select options={languages} value={userLang}
 					onChange={(e) => setUserLang(e.value)}
 					placeholder={userLang} />
